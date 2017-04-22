330723eb1dd820acebfb038b5b1548b4bd86ab70
Vadim Shabanov <vadim.shabanov@gmail.com>
2017-04-22 15:28:07 +0300

    cleaning

--- server/src/main/java/com/vdshb/security/SecurityTokensService.java
+++ server/src/main/java/com/vdshb/security/SecurityTokensService.java
@@ -1,15 +1,12 @@
 package com.vdshb.security;
 
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.time.Instant;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class SecurityTokensService {
@@ -22,15 +19,6 @@ public class SecurityTokensService {
 
     @Inject
     private SecurityUserRepository securityUserRepository;
-
-    //todo: add caching
-//    private final Cache<String, SecurityUserToken> cachedSecurityUsers;
-
-//    public SecurityTokensService(@Value("${security.access-token-expiration-time}") Long accessTokenExpirationTime) {
-//        this.cachedSecurityUsers = Caffeine.newBuilder()
-//                .expireAfterWrite(accessTokenExpirationTime, TimeUnit.SECONDS)
-//                .build();
-//    }
 
     public void renewTokens(SecurityUser securityUser) {
         String newAccessToken = UUID.randomUUID().toString();
