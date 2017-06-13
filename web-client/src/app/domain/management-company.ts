3a00f99b8737d48d4d97e88f2955ed5ba93909b0
Vadim Shabanov <vadim.shabanov@gmail.com>
2017-06-14 00:44:18 +0300

    add todo task

--- web-client/src/app/domain/management-company.ts
+++ web-client/src/app/domain/management-company.ts
@@ -1,5 +1,5 @@
 import {Town} from './town';
-//todo: think about renaming. ResourceRegisterer? Company?
+//todo: think about renaming. ResourceRegistrar? Company?
 export class ManagementCompany {
   id?: number;
   name: string;
