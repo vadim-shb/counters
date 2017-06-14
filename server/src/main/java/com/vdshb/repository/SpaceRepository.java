5ce3feb98b20973cb3e01c8f0793b47485ad80fa
Vadim Shabanov <vadim.shabanov@gmail.com>
2017-06-15 00:44:21 +0300

    bug fix

--- server/src/main/java/com/vdshb/repository/SpaceRepository.java
+++ server/src/main/java/com/vdshb/repository/SpaceRepository.java
@@ -2,12 +2,13 @@ package com.vdshb.repository;
 
 import com.vdshb.domain.Space;
 import com.vdshb.util.SoftDeleteCrudRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SpaceRepository extends SoftDeleteCrudRepository<Space, Long> {
 
-    //fixme: not load inactive
+    @Query("select e from Space e where e.isActive = true and e.userId = ?1")
     List<Space> findByUserId(Long userId);
 
 }
